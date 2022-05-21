c05ac96c165c26763fd3ee5b93b28b9f73b9185f
DucHoang-UIT <jad410.00@gmail.com>
2022-05-21 15:52:53 +0700

    chore

--- src/main/java/controllers/tabs/SettingController.java
+++ src/main/java/controllers/tabs/SettingController.java
@@ -1,4 +1,24 @@
 package controllers.tabs;
 
+import com.jfoenix.controls.JFXTabPane;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
 public class SettingController {
+    @FXML
+    private Tab tabTypeSaveMoney;
+    @FXML
+    private Tab tabOtherInfo;
+    @FXML
+    private Tab tabEmployee;
+    @FXML
+    private JFXTabPane tabSettingPanel;
+    private  int  _currentTab = 0;
+
+    @FXML
+    void initialize(){
+        System.out.println("khoi tao");
+    }
 }
